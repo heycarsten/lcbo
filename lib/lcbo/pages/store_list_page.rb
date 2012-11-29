4b29993cdc2b02f5b31096344b4d96e007f5185c
Carsten Nielsen <heycarsten@gmail.com>
2012-11-29 00:26:30 -0500

    Updated store list page to use new XML-based store locator API.

--- lib/lcbo/pages/store_list_page.rb
+++ lib/lcbo/pages/store_list_page.rb
@@ -3,41 +3,16 @@ module LCBO
 
     include CrawlKit::Page
 
-    STORE_COUNT_RANGE = 595..630
+    STORE_COUNT_RANGE = 600..635
 
     on :after_parse, :verify_number_of_stores
 
-    http_method :post
-    uri 'http://www.lcbo.com/lcbo-ear/lcbo/store/searchResults.do'
-
-    default_body_params \
-      :language               => 'EN',
-      :searchType             => 'proximity',
-      :numstores              => '999',
-      :streetNumber           => '',
-      :streetName             => '',
-      :streetType             => '',
-      :streetDirection        => '',
-      :municipality_proximity => '',
-      :postalCode             => 'M5V1K1',
-      :'Find Stores.x'        => '68',
-      :'Find Stores.y'        => '9',
-      :municipality_citywide  => ''
+    uri 'http://www.lcbo.com/lcbo-webapp/storequery.do?searchType=proximity&longitude=-79.0&latitude=43.0&numstores=999&language=EN'
 
     emits :store_ids do
       @store_ids ||= begin
-        anchors.reduce([]) { |ary, a|
-          if (match = a.attribute('href').value.match(/\&STORE=([0-9]+)/))
-            ary << match.captures[0].to_i
-          else
-            next ary
-          end
-        }.sort
+        doc.xpath('//store/locationnumber').map { |node| node.content.to_i }
       end
-    end
-
-    def anchors
-      doc.css('.store-location-list td[width="14%"] a.item-details-col0')
     end
 
     def verify_number_of_stores
