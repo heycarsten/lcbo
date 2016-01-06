f29981dc4dbee82fb4655c0182024889f6e2ad98
lenard <lenardandal@gmail.com>
2016-01-06 10:41:53 -0500

    do not throw error when lcbo inventory page returns 0 results

--- lib/lcbo/pages/inventory_page.rb
+++ lib/lcbo/pages/inventory_page.rb
@@ -14,13 +14,17 @@ module LCBO
     end
 
     emits :inventories do
-      # [updated_on, store_id, quantity]
-      doc.css('table[cellpadding="5"] tr')[1..-1].map do |tr|
-        {
-          updated_on: Date.parse(tr.css('td')[2].content).to_s,
-          store_id: tr.css('td')[1].css('a').attr("href").content.match(/STORE=(\d+)/)[1].to_i,
-          quantity: tr.css('td')[3].content.strip.to_i,
-        }
+      if doc.css('input[name="inventorySize"]').attr('value').value.to_i.zero?
+        []
+      else
+        # [updated_on, store_id, quantity]
+        doc.css('table[cellpadding="5"] tr')[1..-1].map do |tr|
+          {
+            updated_on: Date.parse(tr.css('td')[2].content).to_s,
+            store_id: tr.css('td')[1].css('a').attr("href").content.match(/STORE=(\d+)/)[1].to_i,
+            quantity: tr.css('td')[3].content.strip.to_i,
+          }
+        end
       end
     end
 
